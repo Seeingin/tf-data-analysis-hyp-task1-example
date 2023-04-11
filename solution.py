bfe63d0a137b24c494387b47e9ff9ce740d5c607
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-11 13:19:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,15 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    x = x_success / x_cnt
-    y = y_success / y_cnt
-    if y - x > 0:
+
+    import scipy.stats as stats
+
+    p_hat = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (y_success/y_cnt - x_success/x_cnt) / (p_hat * (1 - p_hat) * (1/x_cnt + 1/y_cnt)) ** 0.5
+    p_value = 1 - stats.norm.cdf(z)
+
+    if p_value < 0.05:
       res = False
-    if y - x < 0:
+    else:
       res = True
     return res # Ваш ответ, True или False
